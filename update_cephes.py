73e104fb6d53cc427d306f78c1e718d7a29f1334
Danilo Horta <danilo.horta@gmail.com>
2016-04-21 17:13:59 +0100

    cephes downloaded

---
+++ update_cephes.py
@@ -0,0 +1,23 @@
+from six.moves.urllib.request import urlopen
+import shutil
+import os
+import re
+import tarfile
+
+url = "http://www.netlib.org/cephes/"
+page = urlopen(url).read()
+
+c = re.compile(r'"([^"]+\.tgz)"')
+pkgs = re.findall(c, page)
+
+curdir = os.path.abspath(os.path.dirname(__file__))
+
+for pkg in pkgs:
+    # content = urllib.urlopen(url + pkg).read()
+    stream = urlopen(url + pkg)
+    content = tarfile.open(fileobj=stream, mode="r|gz")
+    name = os.path.splitext(pkg)[0]
+    path = os.path.join(curdir, 'ncephes', 'cephes', name)
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    content.extractall(path)
