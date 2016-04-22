6c6f4a4ff734efc66be2ec9fe2cedad2255344e2
Danilo Horta <danilo.horta@gmail.com>
2016-04-22 02:36:58 +0100

    calling inside nopython

--- ncephes/test/test_numba.py
+++ ncephes/test/test_numba.py
@@ -2,14 +2,10 @@ from ncephes import cprob
 from numba import jit
 import unittest
 
-numba_incbet = cprob.incbet
-
+_incbet = cprob.incbet
 @jit(nogil=True, nopython=True)
-def _incbet(a, b, x):
-    # return 0.657
-    # return cprob.incbet(a, b, x)
-    # return cprob.numba_incbet(a, b, x)
-    return numba_incbet(a, b, x)
+def incbet(a, b, x):
+    return _incbet(a, b, x)
 
 @jit(nogil=True)
 def erf(x):
@@ -17,7 +13,7 @@ def erf(x):
 
 class TestNumba(unittest.TestCase):
     def test_incbet(self):
-        self.assertAlmostEqual(_incbet(1., 3., 0.3), 0.657)
+        self.assertAlmostEqual(incbet(1., 3., 0.3), 0.657)
 
     def test_erf(self):
         self.assertAlmostEqual(cprob.erf(1.3), 0.9340079449406524)
