c482485ec339d3b815d81bdffb18c326415051e6
Danilo Horta <danilo.horta@gmail.com>
2016-04-23 21:51:09 +0100

    Preparing release 0.0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 pkg_name = 'ncephes'
-version  = '0.0.5.dev0'
+version = '0.0.5'
 
 def get_test_suite():
     from unittest import TestLoader
