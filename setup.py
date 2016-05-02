db16657b3be7526f08e822a923abfcbb25da3cb0
Danilo Horta <danilo.horta@gmail.com>
2016-05-02 22:06:04 +0100

    Back to development: 0.0.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 pkg_name = 'ncephes'
-version = '0.0.7'
+version = '0.0.8.dev0'
 
 def get_test_suite():
     from unittest import TestLoader
