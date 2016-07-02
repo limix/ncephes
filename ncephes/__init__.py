33f2f762080562fc309b02bd5359c7313c826e67
Danilo Horta <danilo.horta@gmail.com>
2016-07-02 12:57:28 +0100

    version

--- ncephes/__init__.py
+++ ncephes/__init__.py
@@ -1,5 +1,11 @@
 from __future__ import absolute_import
-import pkg_resources
-from .lib import get_include
 
-__version__ = pkg_resources.get_distribution("ncephes").version
+import pkg_resources
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except pkg_resources.DistributionNotFound:
+    __version__ = 'unknown'
+
+from .lib import get_include
+from . import cprob
+from . import ellf
