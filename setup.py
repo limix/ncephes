a9d82e70043444fcc1c3837b082aedc9990e7317
Danilo Horta <danilo.horta@gmail.com>
2016-08-18 13:47:25 +0100

    using build_capi

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from module_info import get_extra_compile_args
 from capi_info import get_include_dirs
 
 pkg_name = 'ncephes'
-version = '0.0.10.dev2'
+version = '0.0.10'
 
 
 def _check_pycparser():
