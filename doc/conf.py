6bec4c9d0f68afcf12bfd8a741ddb0ae4260276f
Danilo Horta <danilo.horta@gmail.com>
2016-11-02 17:47:30 +0000

    nature theme

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,6 @@ from __future__ import absolute_import
 
 # import sys
 # import os
-import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -119,7 +118,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
