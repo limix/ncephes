0b8f99dbb4c8cf0a015e16804f8d08990384ea65
Danilo Horta <danilo.horta@gmail.com>
2016-11-13 10:59:34 +0000

    try numba

--- ncephes/cprob.py
+++ ncephes/cprob.py
@@ -1,4 +1,7 @@
 from . import _cprob_ffi
 from ._cprob_ffi.lib import *
-from numba import cffi_support as _cffi_support
-_cffi_support.register_module(_cprob_ffi)
+try:
+    from numba import cffi_support as _cffi_support
+    _cffi_support.register_module(_cprob_ffi)
+except ImportError:
+    pass
--- ncephes/misc.py
+++ ncephes/misc.py
@@ -1,4 +1,8 @@
 from . import _misc_ffi
 from ._misc_ffi.lib import *
-from numba import cffi_support as _cffi_support
-_cffi_support.register_module(_misc_ffi)
+
+try:
+    from numba import cffi_support as _cffi_support
+    _cffi_support.register_module(_misc_ffi)
+except ImportError:
+    pass
