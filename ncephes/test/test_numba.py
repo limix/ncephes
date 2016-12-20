906869c25c05bbda199d249e3d42b930236bb921
Danilo Horta <danilo.horta@gmail.com>
2016-12-20 15:20:03 +0000

    numba

--- ncephes/test/test_numba.py
+++ ncephes/test/test_numba.py
@@ -4,22 +4,19 @@ from ncephes import cprob
 from ncephes.cprob import incbet
 from numpy.testing import assert_almost_equal
 
-try
+try:
     from numba import jit
 
     @jit(nogil=True, nopython=True)
     def _incbet(a, b, x):
         return incbet(a, b, x)
 
-
     @jit(nogil=True)
     def erf(x):
         return cprob.erf(x)
 
-
     def test_incbet():
         assert_almost_equal(_incbet(1., 3., 0.3), 0.657)
-
 
     def test_erf():
         assert_almost_equal(erf(1.3), 0.9340079449406524)
