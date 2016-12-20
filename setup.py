e2d5fcaf62279ecbe96e6231f850f3650d61a097
Danilo Horta <danilo.horta@gmail.com>
2016-12-20 17:03:11 +0000

    release

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ def setup_package():
 
     metadata = dict(
         name='ncephes',
-        version='1.0.17.dev1',
+        version='1.0.17',
         maintainer="Danilo Horta",
         maintainer_email="danilo.horta@gmail.com",
         description="Python interface for the Cephes library.",
