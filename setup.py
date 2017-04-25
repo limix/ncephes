bb403b4b9084ff000d3ba5ecf147a3e40287bf81
Danilo Horta <danilo.horta@gmail.com>
2017-04-25 18:11:18 +0100

    Readme update

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ def setup_package():
     setup_requires = ['build-capi', 'cffi>=1.7', 'pycparser'] + pytest_runner
     install_requires = ['cffi>=1.7']
     tests_require = ['numpy', 'pytest>=3']
-    recommended = {"numba": ["numba>=0.28"]}
+    recommended = {"numba": ["numba>=0.30"]}
 
     modules = open('supported_modules.txt').read().split("\n")[:-1]
     cffi_modules = ['module_build.py:%s' % m for m in modules]
