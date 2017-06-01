e457c37352acb8c041c17075acc4b3f6a586e7cd
Danilo Horta <danilo.horta@gmail.com>
2017-06-01 08:42:26 +0100

    Require six

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ def setup_package():
 
     metadata = dict(
         name='ncephes',
-        version='1.0.39',
+        version='1.0.40',
         maintainer="Danilo Horta",
         maintainer_email="danilo.horta@gmail.com",
         description="Python interface for the Cephes library.",
