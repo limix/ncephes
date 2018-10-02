9e1941cbb093f810814ba70e3daccf9e30f0bd88
Danilo Horta <danilo.horta@gmail.com>
2018-10-02 10:14:01 +0100

    doc

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ napoleon_google_docstring = True
 templates_path = ["_templates"]
 source_suffix = ".rst"
 master_doc = "index"
-project = u"nCephes"
+project = u"Ncephes"
 copyright = u"2016, Danilo Horta"
 author = u"Danilo Horta"
 version = get_version()
@@ -40,20 +40,4 @@ todo_include_todos = False
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 htmlhelp_basename = "ncephes-doc"
-latex_elements = {}
-latex_documents = [
-    (master_doc, "nCephes.tex", u"nCephes Documentation", u"Danilo Horta", "manual")
-]
-man_pages = [(master_doc, "ncephes", u"nCephes Documentation", [author], 1)]
-texinfo_documents = [
-    (
-        master_doc,
-        "nCephes",
-        u"nCephes Documentation",
-        author,
-        "nCephes",
-        "One line description of project.",
-        "Miscellaneous",
-    )
-]
 intersphinx_mapping = {"https://docs.python.org/": None}
